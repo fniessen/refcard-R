6a69293b33282e826798b09ea398ec71410be2a3
Fabrice Niessen <(concat "fniessen" at-sign "pirilampo.org")>
2015-05-12 11:30:29 +0200

    Update "Contributing"

--- Rlib.R
+++ Rlib.R
@@ -1,6 +1,6 @@
 #  Rlib.R -- Collection of utility and convenience functions
 #
-#  Copyright (C) 2014-2014 Fabrice Niessen
+#  Copyright (C) 2014-2015 Fabrice Niessen
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
