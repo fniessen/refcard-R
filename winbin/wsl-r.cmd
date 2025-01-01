5de126212330bb65f396a9b0bb866015fd245041
Fabrice Niessen <fni@pirilampo.be>
2025-01-01 17:53:16 +0100

    Update wsl-r.cmd with detailed comments for usage and placement
    - Added comments at the top of wsl-r.cmd to provide instructions on usage and
      where to place the script.
    - Explained how to invoke the R interpreter installed in WSL and passed
      arguments.

--- winbin/wsl-r.cmd
+++ winbin/wsl-r.cmd
@@ -1,2 +1,17 @@
 @echo off
+:: This script is used to invoke the R interpreter installed in WSL (Windows
+:: Subsystem for Linux).
+::
+:: Place this script in a directory included in your system's PATH (e.g.,
+:: C:\Program Files).
+::
+:: Usage:
+::     Call this script from the Windows command prompt, passing any desired
+::     arguments for R.
+::
+:: Example:
+::     wsl-r.cmd --version
+::
+:: Arguments provided will be forwarded to the R interpreter in WSL.
+
 wsl /usr/bin/R %*
